a9d1d007a0cf216ccb1e8d9b566992ef16cadbca
Adams Zago <adams.zago@gmail.com>
2019-06-23 13:04:10 -0300

    Includind lista Convidados and form to create new Convidado

--- src/main/java/com/zago/lista/ConvidadoController.java
+++ src/main/java/com/zago/lista/ConvidadoController.java
@@ -1,16 +1,31 @@
 package com.zago.lista;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.zago.lista.entities.Convidado;
+import com.zago.lista.repository.ConvidadoRepository;
 
 
 @Controller
 public class ConvidadoController {
 
+	@Autowired
+	ConvidadoRepository repository;
+	
     @RequestMapping("/")
     public String index(){
         return "index";
+    }
+    
+    @RequestMapping("listaconvidados")
+    public String listaConvidados(Model model) {
+    	Iterable<Convidado> convidados = repository.findAll();
+    	model.addAttribute("convidados", convidados);
+    	return "listaconvidados";
     }
 
 }
---
+++ src/main/java/com/zago/lista/repository/ConvidadoRepository.java
@@ -0,0 +1,10 @@
+package com.zago.lista.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.zago.lista.entities.Convidado;
+
+public interface ConvidadoRepository extends CrudRepository<Convidado, Long>{
+
+	
+}
